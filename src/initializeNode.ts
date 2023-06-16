7f5d8ceaf74b9005c23687aa5a5e64f5cc084c7f
naokiy <to_contact@naokiy.net>
2023-06-16 21:09:50 +0000

    :recycle: Refactor

--- src/initializeNode.ts
+++ src/initializeNode.ts
@@ -9,12 +9,12 @@ export function initializeNode<
 >(
   name: string,
   RED: NodeAPI,
-  cls: new (node: Node<TCred>, config: TConfig) => TOperator
+  operatorConstructor: new (node: Node<TCred>, config: TConfig) => TOperator
 ) {
   const node = function (this: Node<TCred>, nodeDef: NodeDef & TConfig) {
+    const op = new operatorConstructor(this, nodeDef);
     RED.nodes.createNode(this, nodeDef);
-    const op = new cls(this, nodeDef);
-    this.on("input", (message) => op.onInput(message));
+    op.postCreate(this);
   };
 
   RED.nodes.registerType(name, node);
