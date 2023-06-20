5ae110e889bc388f58107af628e55953e2e89b21
naokiy <to_contact@naokiy.net>
2023-06-20 21:04:35 +0900

    🔧  Replace test library (#19)
    * :wrench: Change test lib from esbuild-jest to @swc/jest
    
    * :recycle: Refactor

--- src/ip.spec.ts
+++ src/ip.spec.ts
@@ -2,18 +2,16 @@ import ipNode from "./ip";
 
 import helper from "node-red-node-test-helper";
 
-describe('IP Node', () => {
-    afterEach(async () => {
-        await helper.unload();
+describe("IP Node", () => {
+  afterEach(async () => {
+    await helper.unload();
+  });
+
+  test("Should be loaded", async () => {
+    const flow = [{ id: "n1", type: "ip", name: "test name" }];
+    await helper.load(ipNode, flow, () => {
+      const n1 = helper.getNode("n1");
+      expect(n1.name).toBe("test name");
     });
-    
-    test("Should be loaded", async() => {
-        const flow = [{id: "n1", type: "ip", name: "test name"}];
-        await helper.load(ipNode, flow, () => {
-            const n1  = helper.getNode("n1");
-            expect(n1.name).toBe("test name");
-        });
-    });
+  });
 });
-
-
